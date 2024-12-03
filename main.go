69ce5e5c90d79b01f85cb381cc422d721f7a7b80
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2024-12-03 10:21:16 +0530

    createAuthor function created and added some error logs

--- main.go
+++ main.go
@@ -19,14 +19,26 @@ const (
 var db *sql.DB
 var err error
 
+// User creation
 func createUser(mail, username, password, salt string) (userID int, err error) {
 	query := ` INSERT INTO users(mail,username,password,salt) VALUES($1,$2,$3,$4) RETURNING id `
 
 	if err := db.QueryRow(query, mail, username, password, salt).Scan(&userID); err != nil {
-		return 0, err
+		return 0, fmt.Errorf("user creation failed due to : %v", err)
 	}
 
 	return userID, nil
+}
+
+// Author creation
+func createAuthor(name string, createdBy int) (authorID int, err error) {
+	query := ` INSERT INTO authors(name,created_by) VALUES($1,$2) RETURNING id`
+
+	if err := db.QueryRow(query, name, createdBy).Scan(&authorID); err != nil {
+		return 0, fmt.Errorf("author creation failed due to : %v", err)
+	}
+
+	return authorID, nil
 }
 
 func main() {
@@ -48,11 +60,21 @@ func main() {
 	log.Println("Connected to db successfully...")
 
 	// Email, Username, Password, Salt
-	userID, err := createUser("random@gmail.com", "randomUsername", "randomPassword", "randomSalt")
+	userID, err := createUser("random2@gmail.com", "random2Username", "random2Password", "random2Salt")
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
 	log.Printf(" User created with ID: %d", userID)
+
+	// Author name
+	authorID, err := createAuthor("random2author name", 5)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	
+	log.Printf("Author created with ID : %d ", authorID)
 
 }
