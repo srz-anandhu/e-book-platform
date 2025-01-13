1afb9bb111dd9cdf859a492aa0f0b9d36a5b08f0
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-01-13 12:57:17 +0530

    user controller completed

--- app/controller/user_controller.go
+++ app/controller/user_controller.go
@@ -9,6 +9,10 @@ import (
 
 type UserController interface {
 	GetOne(w http.ResponseWriter, r *http.Request)
+	CreateUser(w http.ResponseWriter, r *http.Request)
+	UpdateUser(w http.ResponseWriter, r *http.Request)
+	DeleteUser(w http.ResponseWriter, r *http.Request)
+	GetAllUsers(w http.ResponseWriter, r *http.Request)
 }
 
 type UserControllerImpl struct {
@@ -31,4 +35,42 @@ func (c *UserControllerImpl) GetOne(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	api.Success(w, http.StatusOK, userResp)
+}
+
+func (c *UserControllerImpl) CreateUser(w http.ResponseWriter, r *http.Request) {
+	result, err := c.userService.CreateUser(r)
+	if err != nil {
+		httpErr := e.NewAPIError(err, "user creation failed")
+		api.Fail(w, httpErr.StatusCode, httpErr.Code, httpErr.Message, err.Error())
+		return
+	}
+	api.Success(w, http.StatusCreated, result)
+}
+
+func (c *UserControllerImpl) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	if err := c.userService.UpdateUser(r); err != nil {
+		httpErr := e.NewAPIError(err, "user updation failed")
+		api.Fail(w, httpErr.StatusCode, httpErr.Code, httpErr.Message, err.Error())
+		return
+	}
+	api.Success(w, http.StatusOK, "user updated successfully")
+}
+
+func (c *UserControllerImpl) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	if err := c.userService.DeleteUser(r); err != nil {
+		httpErr := e.NewAPIError(err, "can't delete user")
+		api.Fail(w, httpErr.StatusCode, httpErr.Code, httpErr.Message, err.Error())
+		return
+	}
+	api.Success(w, http.StatusOK, "deleted user")
+}
+
+func (c *UserControllerImpl) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	result, err := c.userService.GetAllUsers()
+	if err != nil {
+		httpErr := e.NewAPIError(err, "can't get all users")
+		api.Fail(w, httpErr.StatusCode, httpErr.Code, httpErr.Message, err.Error())
+		return
+	}
+	api.Success(w, http.StatusOK, result)
 }
