bbcb702399f580fbf627635b2998d836d7185e15
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-01-14 12:30:43 +0530

    author service completed

--- app/dto/author.go
+++ app/dto/author.go
@@ -89,6 +89,29 @@ func (a *AuthorUpdateRequest) Validate() error {
 }
 
 type AuthorDeleteReq struct {
-	ID int `validate:"required"`
+	ID        int `validate:"required"`
 	DeletedBy int `validate:"required"` // userID
+}
+
+func (a *AuthorDeleteReq) Parse(r *http.Request) error {
+	strID := chi.URLParam(r, "id")
+	intID, err := strconv.Atoi(strID)
+	if err != nil {
+		return err
+	}
+	a.ID = intID
+
+	// Decode to AuthorDeleteReq
+	if err := json.NewDecoder(r.Body).Decode(&a); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *AuthorDeleteReq) Validate() error {
+	validate := validator.New()
+	if err := validate.Struct(a); err != nil {
+		return err
+	}
+	return nil
 }
--- app/service/author_service.go
+++ app/service/author_service.go
@@ -1,1 +1,128 @@
 package service
+
+import (
+	"ebook/app/dto"
+	"ebook/app/repo"
+	"net/http"
+)
+
+type AuthorService interface {
+	GetAuthor(r *http.Request) (*dto.AuthorResponse, error)
+	CreateAuthor(r *http.Request) (int64, error)
+	UpdateAuthor(r *http.Request) error
+	DeleteAuthor(r *http.Request) error
+	GetAllAuthors() ([]*dto.AuthorResponse, error)
+}
+
+type AuthorServiceImpl struct {
+	authorRepo repo.AuthorRepo
+}
+
+// For checking implementation of Service interface
+var _ AuthorService = (*AuthorServiceImpl)(nil)
+
+func NewAuthorService(authorRepo repo.AuthorRepo) AuthorService {
+	return &AuthorServiceImpl{
+		authorRepo: authorRepo,
+	}
+}
+
+func (s *AuthorServiceImpl) GetAuthor(r *http.Request) (*dto.AuthorResponse, error) {
+	req := &dto.AuthorRequest{}
+	if err := req.Parse(r); err != nil {
+		return nil, err
+	}
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	resp, err := s.authorRepo.GetAuthor(req.ID)
+	if err != nil {
+		return nil, err
+	}
+	var author dto.AuthorResponse
+
+	author.ID = resp.ID
+	author.Name = resp.Name
+	author.Status = resp.Status
+	author.CreatedAt = resp.CreatedAt
+	author.CreatedBy = resp.CreatedBy
+	author.UpdatedAt = resp.UpdatedAt
+	author.UpdatedBy = resp.UpdatedBy
+	author.DeletedAt = resp.DeletedAt
+	author.DeletedBy = resp.DeletedBy
+
+	return &author, nil
+}
+
+func (s *AuthorServiceImpl) CreateAuthor(r *http.Request) (int64, error) {
+	body := &dto.AuthorCreateRequest{}
+
+	if err := body.Parse(r); err != nil {
+		return 0, err
+	}
+	if err := body.Validate(); err != nil {
+		return 0, err
+	}
+	authorID, err := s.authorRepo.CreateAuthor(body)
+	if err != nil {
+		return 0, err
+	}
+	return authorID, nil
+}
+
+func (s *AuthorServiceImpl) UpdateAuthor(r *http.Request) error {
+	body := &dto.AuthorUpdateRequest{}
+
+	if err := body.Parse(r); err != nil {
+		return err
+	}
+	if err := body.Validate(); err != nil {
+		return err
+	}
+	if err := s.authorRepo.UpdateAuthor(body); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *AuthorServiceImpl) DeleteAuthor(r *http.Request) error {
+	body := &dto.AuthorDeleteReq{}
+
+	if err := body.Parse(r); err != nil {
+		return err
+	}
+	if err := body.Validate(); err != nil {
+		return err
+	}
+	if err := s.authorRepo.DeleteAuthor(body); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *AuthorServiceImpl) GetAllAuthors() ([]*dto.AuthorResponse, error) {
+	results, err := s.authorRepo.GetAllAuthors()
+	if err != nil {
+		return nil, err
+	}
+	var authors []*dto.AuthorResponse
+
+	for _, val := range results {
+
+		var author dto.AuthorResponse
+
+		author.ID = val.ID
+		author.Name = val.Name
+		author.Status = val.Status
+		author.CreatedBy = val.CreatedBy
+		author.CreatedAt = val.CreatedAt
+		author.UpdatedBy = val.UpdatedBy
+		author.UpdatedAt = val.UpdatedAt
+		author.DeletedBy = val.DeletedBy
+		author.DeletedAt = val.DeletedAt
+
+		authors = append(authors, &author)
+	}
+
+	return authors, nil
+}
