14128809520fceaecf6c7933e1ee1131c8f8f8e4
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-01-17 10:03:02 +0530

    routes completed (user, author, book)

--- app/routes.go
+++ app/routes.go
@@ -17,13 +17,43 @@ func APIRouter(db *gorm.DB) chi.Router {
 	// 	r.Get("/hello", api.DemoHandler)
 	// })
 
+	// User
 	userRepo := repo.NewUserRepo(db)
 	userService := service.NewUserService(userRepo)
 	userController := controller.NewUserController(userService)
 
+	// Author
+	authorRepo := repo.NewAuthorRepo(db)
+	authorService := service.NewAuthorService(authorRepo)
+	authorController := controller.NewAuthorController(authorService)
+
+	// Book
+	bookRepo := repo.NewBookRepo(db)
+	bookService := service.NewBookService(bookRepo)
+	bookController := controller.NewBookController(bookService)
+
 	r.Route("/users", func(r chi.Router) {
 		r.Get("/", userController.GetAllUsers)
 		r.Post("/create", userController.CreateUser)
+		r.Put("/{id}", userController.UpdateUser)
+		r.Delete("/{id}", userController.DeleteUser)
+		r.Get("/{id}", userController.GetOne)
+	})
+
+	r.Route("/authors", func(r chi.Router) {
+		r.Get("/", authorController.GetAllAuthors)
+		r.Post("/create", authorController.CreateAuthor)
+		r.Put("/{id}", authorController.UpdateAuthor)
+		r.Delete("/{id}", authorController.DeleteAuthor)
+		r.Get("/{id}", authorController.GetAuthor)
+	})
+
+	r.Route("/books", func(r chi.Router) {
+		r.Get("/", bookController.GetAllBooks)
+		r.Post("/create", bookController.CreateBook)
+		r.Put("/{id}", bookController.UpdateBook)
+		r.Delete("/{id}", bookController.DeleteBook)
+		r.Get("/{id}", bookController.GetBook)
 	})
 
 	return r
